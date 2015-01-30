c8e240578c470e82c57771f314bdbd5d7847f9bd
Brock Ellis <brock@sharproot.com>
2015-01-30 16:56:10 -0600

    Working on getting OAuth working

--- js/main.js
+++ js/main.js
@@ -12,11 +12,32 @@ var api = new function(){
         console.log("Attempting to call API - post");
         console.log("Endpoint: " + endpoint);
         return $.post(base + endpoint, data);
+    },
+    
+    redirect = function(){
+        var path = 'https://app.sycamoreeducation.com/oauth/authorize.php?';
+        var queryParams = ['client_id=' + CONFIG.client_id,
+        'redirect_uri=' + CONFIG.callback_uri,
+        'scope=general open individual',
+        'response_type=token'];
+        var query = queryParams.join('&');
+        var url = path + query;
+        window.open(url,"_self"); 
+    },
+    
+    verifyOAuth = function(){
+        var rawParams = window.location.hash.substring(1);
+        var oAuthData = parseParams(rawParams);
+        setLocalStorage(oAuthData[0]);
+                
+        window.open("classes.html", "_self");  
     };
     
     return {
         getData: getData,
-        postData: postData
+        postData: postData,
+        redirect: redirect,
+        verifyOAuth: verifyOAuth
     };
     
 } ();
@@ -51,4 +72,12 @@ $.ajaxSetup({
         var access_token = CONFIG.access_code;
         xhr.setRequestHeader('Authorization', 'Bearer '+access_token);
     }
+});
+
+$(".logout").click(function(){
+    alert("clicked");
+    
+    window.location.replace("index.html");
+    localStorage.clear();
+
 });
